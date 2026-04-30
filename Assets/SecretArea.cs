96a9cf77cbeb1a4b8057ed3fe7d08e54f8028145
aisling lu <aislinglu2@gmail.com>
2026-04-30 14:47:42 -0700

    secret passage mechanism

---
+++ Assets/SecretArea.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using UnityEngine;
+
+public class SecretArea : MonoBehaviour
+{
+    public float fadeDuration = 1;
+
+    SpriteRenderer[] spriteRenderers;
+    Color[] hiddenColors;
+    Coroutine currentCoroutine;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        hiddenColors = new Color[spriteRenderers.Length];
+        for (int i = 0; i <spriteRenderers.Length; i++)
+        {
+            hiddenColors[i] = spriteRenderers[i].color;
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (currentCoroutine != null)
+            {
+                StopCoroutine(currentCoroutine);
+            }
+            currentCoroutine = StartCoroutine(FadeSprite(true));
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log($"activeInHierarchy: {gameObject.activeInHierarchy}, activeSelf: {gameObject.activeSelf}");
+
+            if (currentCoroutine != null)
+            {
+                StopCoroutine(currentCoroutine);
+            }
+            currentCoroutine = StartCoroutine(FadeSprite(false));
+        }
+    }
+
+    private IEnumerator FadeSprite(bool fadeOut)
+    {
+        Color[] startColors = new Color[spriteRenderers.Length];
+        for (int i = 0; i < spriteRenderers.Length; i++)
+        {
+            startColors[i] = spriteRenderers[i].color;
+        }
+
+        float timeFading = 0f;
+
+        while (timeFading < fadeDuration)
+        {
+            for (int i = 0; i < spriteRenderers.Length; i++)
+            {
+                Color target = fadeOut ? new Color(hiddenColors[i].r, hiddenColors[i].g, hiddenColors[i].b, 0f) : hiddenColors[i];
+                spriteRenderers[i].color = Color.Lerp(startColors[i], target, timeFading / fadeDuration);
+            }
+            
+            timeFading += Time.deltaTime;
+            yield return null;
+        }
+
+        for (int i = 0; i < spriteRenderers.Length; i++) {
+            spriteRenderers[i].color = fadeOut ? new Color(hiddenColors[i].r, hiddenColors[i].g, hiddenColors[i].b, 0f) : hiddenColors[i];
+        }
+    }
+}
